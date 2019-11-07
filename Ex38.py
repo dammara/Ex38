83275e1667436eef444dfcdb6dad1f27433cef76
dammara <h3246531>
2019-11-06 19:49:53 -0500

    11/6/19 7:49PM - wrote and runs code, runs successfully.

--- Ex38.py
+++ Ex38.py
@@ -1,1 +1,27 @@
+# Markhus Dammar
+# 11/6/19
+# This program works with lists
 
+ten_things = "Apples Oranges Crows Telephone Light Sugar"
+
+print("Wait there are not ten things in that list. Let's fix that")
+
+stuff = ten_things.split(' ')
+more_stuff = ["Day", "Night", "Song", "Frisbee",
+              "Corn", "Banana", "Girl", "Boy"]
+
+while len(stuff) != 10:
+    next_one = more_stuff.pop()
+    print("Adding", next_one)
+    stuff.append(next_one)
+    print(f"there are {len(stuff)} items now")
+
+print("There we go", stuff)
+
+print("Let's do some things with stuff")
+
+print(stuff[1])
+print(stuff[-1])  # woah ! fancy
+print(stuff.pop())
+print(' '.join(stuff)) # what? Cool
+print('#'.join(stuff[3:5]))  # super stellar
